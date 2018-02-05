9601f17e60d04e8eb23ca85f49f9a68e7200ac78
Giuseppe Fechio <gfechio@brascomm.net>
2018-02-05 09:56:16 -0200

    uploading file CurlHelper

---
+++ CurlHelper.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Description of CurlHelper
+ *
+ * @author Giuseppe Dezute Fechio <giuseppe.fechio@hotmail.net>
+ *
+ * Version: 1.0.1
+ */
+class CurlHelper {
+
+    public $ch;
+
+    public function response($url) {
+        curl_setopt($this->ch, CURLOPT_URL, $url);
+        $resposta = curl_exec($this->ch);
+        curl_close($this->ch);
+        return $resposta;
+    }
+
+    public function init() {
+        $this->ch = curl_init();
+        curl_setopt($this->ch, CURLOPT_HEADER, 0);
+        curl_setopt($this->ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+        return $this;
+    }
+
+    public function setPut($data) {
+        curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($this->ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        return $this;
+    }
+
+    public function setPost($data) {
+        curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($this->ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        return $this;
+    }
+
+}
